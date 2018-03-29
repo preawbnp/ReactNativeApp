c7216d35dc36f29e84a4238da9e6b1fd28b4d551
preawbnp <preawbnp@preawbnps-MacBook-Pro.local>
2018-03-29 13:53:17 +0700

    Added KeyboardAvoidingView when using keyboard

--- screen/RegisterScreen.js
+++ screen/RegisterScreen.js
@@ -1,13 +1,50 @@
 import React, { Component } from 'react';
-import { Text, View } from 'react-native';
+import { Text, View, Image, KeyboardAvoidingView } from 'react-native';
+import styles from '../stylesheet/Style'
+import image from '../images/logo.png'
+import { FormLabel, FormInput, Button } from 'react-native-elements';
 
 class RegisterScreen extends Component {
     render() {
         return(
-            <View>
-                <Text>Register Screen</Text>
-            </View>
-        );
+            <KeyboardAvoidingView behavior="padding">
+                <View>
+                    <Image style={styles.logo} source={require('../images/logo.png')}/>
+                    {console.log(styles.logo)}
+                </View>
+                
+                <View style={styles.textContainer}>
+                    <FormLabel>Enter Email</FormLabel>
+                    <FormInput placeholder = "Type your email here"
+                    inputStyle = {{
+                        textAlign: 'center',
+                    }}
+                    containerStyle = {{ 
+                        borderBottomWidth: 0 
+                    }} />
+                </View>
+
+                <View style={styles.textContainer}>
+                    <FormLabel>Enter Password</FormLabel>
+                    <FormInput 
+                        secureTextEntry = { true }
+                        placeholder = "Type your password"
+                        inputStyle = {{
+                            textAlign: 'center',
+                        }} 
+                        containerStyle = {{ 
+                            borderBottomWidth: 0 
+                        }} />
+                </View>
+
+                <View style={styles.buttonContainer}>
+                    <Button
+                        title = "Register"
+                        backgroundColor = "#6caef9"
+                    />
+                </View>
+            </KeyboardAvoidingView>
+        )
     }
 }
 
